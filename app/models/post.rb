bb4dc5ef18c10ad3fa754aa4630d96d5dda8bc24
Jenee Washington <jeneewashington@icloud.com>
2017-01-15 13:49:45 -0500

    Fixed forms and CSS styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   validates :user_id, presence: true
+  validates :caption, length: { minimum: 3, maximum: 300 }
+
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
 
   validates :image, presence: true
 
